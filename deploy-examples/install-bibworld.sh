acbe563b5ba3e3f3bf5a38287e11f3a95d9bfd07
Erik Rodner <Erik.Rodner@uni-jena.de>
2014-01-13 14:30:29 +0100

    direct links

--- deploy-examples/install-bibworld.sh
+++ deploy-examples/install-bibworld.sh
@@ -1,29 +1,29 @@
 #!/bin/sh -x
 
-WEBSERVERGROUPS="nginx"
-BIBWORLDSRC=`pwd`/../
+WEBSERVERGROUP="nginx"
+BIBWORLDSRC=`pwd`
 
-sudo mkdir -p /usr/local/virtualenvs/bibworld
-sudo useradd -r -g $WEBSERVERGROUP -s /bin/false uwsgi
-sudo pip install virtualenvwrapper
+mkdir -p /usr/local/virtualenvs/bibworld
+useradd -r -g $WEBSERVERGROUP -s /bin/false uwsgi
+pip install virtualenvwrapper
 
 export WORKON_HOME=/usr/local/virtualenvs/
 source /usr/bin/virtualenvwrapper.sh 
 mkvirtualenv bibworld
 workon bibworld
 
-sudo pip install flask uwsgi
+pip install flask uwsgi
 
 cd /usr/local/virtualenvs/bibworld/
 git clone $BIBWORLDSRC
 
-sudo chown uwsgi:nginx /usr/local/virtualenvs/bibworld/ -R
-sudo chmod 775 fapuli/fapuliserver.py
+chown uwsgi:nginx /usr/local/virtualenvs/bibworld/ -R
+chmod 775 fapuli/fapuliserver.py
 
-sudo mkdir -p /var/log/uwsgi
-sudo touch /var/log/uwsgi/bibworld.log
+mkdir -p /var/log/uwsgi
+touch /var/log/uwsgi/bibworld.log
 
-sudo /usr/local/virtualenvs/bibworld/bin/uwsgi -x $BIBWORLDSRC/deploy-examples/uwsgi.xml
+/usr/local/virtualenvs/bibworld/bin/uwsgi -x $BIBWORLDSRC/deploy-examples/uwsgi.xml
 
 tail /var/log/uwsgi/bibworld.log
 
