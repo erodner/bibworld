b5acce513ef153f8829bf6323b650a8574c415eb
Erik Rodner <Erik.Rodner@uni-jena.de>
2014-11-20 18:47:40 +0100

    JSON Query Option

--- bib2template.py
+++ bib2template.py
@@ -6,7 +6,7 @@ from pprint import pprint
 import tengjabref
 import tengjinja2
 import re
-
+import json
 
 # Get the BibTeX and template file names
 parser = argparse.ArgumentParser()
@@ -14,6 +14,7 @@ parser.add_argument('-b', help='Source Bibtex file', required=True)
 parser.add_argument('-t', help='HTML template file', required=True)
 parser.add_argument('-o', help='HTML output', default='out.html')
 parser.add_argument('--engine', help='Template engine being used', default='jabref')
+parser.add_argument('--query', help='Query in JSON format', default=None)
 args = parser.parse_args()
 
 bibfile = args.b
@@ -23,15 +24,22 @@ outfn = args.o
 # derive template directory from templatefile
 templatedir = os.path.dirname ( templatefile )
 templatename = os.path.basename ( templatefile )
-m = re.search( '^(\w+)\.', templatename )
+m = re.search( '^([\w-]+)\.', templatename )
 templatename = m.group(1)
 
 mybib = bibdb()
 mybib.readFromBibTex ( bibfile )
 
-refs = mybib.getReferences()
+if args.query is None:
+    jsonquery = {}
+else:
+    with open(args.query,'r') as f:
+        jsonquery = json.load(f)
+
+print jsonquery
+
+refs = mybib.getReferences(**jsonquery)
 print "Number of publications: ", len(refs)
-#pprint(refs)
 
 print "Writing HTML output to", outfn
 if args.engine=='jabref':
