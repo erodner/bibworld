5ad40e061416cd5a0e820cdef28569a7d0555398
Erik Rodner <Erik.Rodner@uni-jena.de>
2014-11-22 18:50:47 +0100

    static templates

--- bib2template.py
+++ bib2template.py
@@ -13,8 +13,11 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-b', help='Source Bibtex file', required=True)
 parser.add_argument('-t', help='HTML template file', required=True)
 parser.add_argument('-o', help='HTML output', default='out.html')
+parser.add_argument('-p', help='PDF and teaser directory', default='.')
+parser.add_argument('-r', help='Root URL', default='')
 parser.add_argument('--engine', help='Template engine being used', default='jabref')
 parser.add_argument('--query', help='Query in JSON format', default=None)
+parser.add_argument('--staticroot', help='Directory for static files that will be specified in the output', default="")
 args = parser.parse_args()
 
 bibfile = args.b
@@ -29,6 +32,16 @@ templatename = m.group(1)
 
 mybib = bibdb()
 mybib.readFromBibTex ( bibfile )
+
+pdfdir = args.p
+mybib.addAuxFiles ( os.path.join( pdfdir, '%s.pdf' ), 'pdf' )
+# compatibility for the old format
+mybib.addAuxFiles ( os.path.join( pdfdir, '%s.pdf.teaser.png' ), 'teaser' )
+mybib.addAuxFiles ( os.path.join( pdfdir, '%s.teaser.png' ), 'teaser', removeIfUnavailable=False )
+mybib.addAuxFiles ( os.path.join( pdfdir, '%s.presentation.pdf' ), 'presentation' )
+mybib.addAuxFiles ( os.path.join( pdfdir, '%s.supplementary.pdf' ), 'supplementary' )
+
+
 
 if args.query is None:
     jsonquery = {}
@@ -45,4 +58,4 @@ print "Writing HTML output to", outfn
 if args.engine=='jabref':
     tengjabref.bib2html( refs, outfn, templatedir, templatename )
 else:
-    tengjinja2.bib2html( refs, outfn, templatedir, templatename )
+    tengjinja2.bib2html( refs, outfn, templatedir, templatename, staticroot=args.staticroot, rooturl=args.r )
--- tengjinja2.py
+++ tengjinja2.py
@@ -2,12 +2,14 @@
 from jinja2 import Template
 from jinja2 import Environment, FileSystemLoader
 
+from functools import partial
+
 # needed for advanced templates with parameters
 #from flask import request, test_request_context
 
 
-def url_for(typeparameter, filename):
-    return filename
+def url_for(typeparameter, filename, staticroot):
+    return staticroot + filename
 
 class requestclass:
     args = {}
@@ -16,13 +18,13 @@ class requestclass:
      
 
 
-def bib2html ( refs, outfn, templatedir, templatename ):
+def bib2html ( refs, outfn, templatedir, templatename, staticroot="", rooturl="" ):
     env = Environment(loader=FileSystemLoader(templatedir))
     request = requestclass(args = {})
-    env.globals['url_for'] = url_for
+    env.globals['url_for'] = partial(url_for, staticroot=staticroot)
     env.globals['request'] = request
     #with test_request_context('/hello', method='POST'):
     template = env.get_template( '%s.html' % (templatename) )
-    rtext = template.render(refs=refs)
+    rtext = template.render(refs=refs, rooturl=rooturl)
     with open(outfn, 'w') as outf:
         outf.write( rtext.encode('utf8') )
